60d7683715fa44493be70c0aa4cb32f47a3e40c2
Tal <tal@server.fake>
2014-08-02 19:03:22 -0600

    Moved get_list_of_devices to LIB file
    Since my get_list_of_devices function was needed for both install.sh
    and the fdisk wrapper script, and they were both the same, keeping
    both synced was starting to be a pain.
    
    Now:
    	install.sh sources the LIB file to use internally
    	install.sh copies the LIB file directly into /usr/local/bin/fdisk
    		This is because I didn't want a separate lib file when
    		the wrapper script was installed

--- install.sh
+++ install.sh
@@ -23,94 +23,10 @@ uid=$(/usr/bin/id -u) && [ "$uid" = "0" ] ||
 [[ -e $SCRIPT_DIR/fdisk ]] ||
 { echo "'fdisk' not found in current directory"; exit 1; }
 
-get_list_of_devices(){
-	for DEVICE in $(cat /proc/partitions  | tail -n+3 | tr -s ' ' | cut -d ' ' -f 5)
-	do
-		#If /dev/$DEVICE is a block device, add /dev prefix to it
-		#If it doesn't exist, skip it
-		[[ -b /dev/$DEVICE ]] && DEVICE=/dev/$DEVICE || continue
-
-		#Ignore partitions
-		if echo $DEVICE | grep -q '^/dev/sd[a-z][0-9][0-9]*'
-		then
-			continue
-		fi
-
-		#Ignore CDs/DVDs
-		if echo $DEVICE | grep -q '^/dev/sr'
-		then
-			continue
-		fi
-
-		#Ignore floppy disks
-		if echo $DEVICE | grep -q '^/dev/fd'
-		then
-			continue
-		fi
-
-		#Do what fdisk does - ignore loop devices directly under /dev
-		$IMITATE_FDISK &&
-		if echo $DEVICE | grep -q '^/dev/loop'
-		then
-			continue
-		fi
-
-		#If $DEVICE has a mapping under /dev/mapper (as determined by 'dmsetup'),
-		#use that instead of the regular /dev/ device
-		#Real fdisk does this too
-		#Note: We don't explicitly check for existence of 'dmsetup' on the system
-		#	because if it doesn't, the command below will quietly fail anyway,
-		#	and we only care if it succeeds
-		if dmsetup info $DEVICE &>/dev/null
-		then
-			DEVICE=/dev/mapper/$(dmsetup info $DEVICE | grep Name | tr -s ' ' | cut -d ' ' -f 2)
-		fi
-
-		#If we aren't trying to imitate fdisk, AND it's a loop device, only skip it
-		#if it doesn't look like it has a partition table
-		$IMITATE_FDISK ||
-		{
-			echo $DEVICE | grep -q loop &&
-			{
-				#Check if the loop device looks like it has a partition table
-				if ! $FDISK_PATH -l $DEVICE 2>/dev/null | grep -q Start
-				then
-					continue
-				fi
-			}
-		}
-
-		#If $DEVICE is a PARTITION on a loop device, ignore it.
-		#Real fdisk doesn't do this check
-		$IMITATE_FDISK ||
-		if echo $DEVICE | grep -q '^/dev/mapper/loop[0-9]'
-		then
-			continue
-		fi
-
-		#If $DEVICE is a logical volume, ignore it. It's basically like running
-		#fdisk on a partition
-		#Real fdisk doesn't do this check
-		#Note: We don't explicitly check for existence of 'lvdisplay' on the system
-		#	because if it doesn't, the command below will quietly fail anyway,
-		#	and we only care if it succeeds
-		$IMITATE_FDISK ||
-		if lvdisplay "$DEVICE" &>/dev/null
-		then
-			continue
-		fi
-
-		#If $DEVICE ends in #p#, it's a partition, so ignore it.
-		#Real fdisk doesn't do this check
-		$IMITATE_FDISK ||
-		if echo $DEVICE | grep -q '[0-9]p[0-9][0-9]*'
-		then
-			continue
-		fi
-
-		echo $DEVICE
-	done
-}
+#Check if the LIB library is available in the same dir
+#as this install.sh script
+[[ -e $SCRIPT_DIR/LIB ]] && . $SCRIPT_DIR/LIB ||
+{ echo "LIB library not found"; exit 1; }
 
 clear
 echo "There are several cases where fdisk runs against partitions, rather than disks,
@@ -221,6 +137,13 @@ $IMITATE_FDISK &&
 	sed -i "s#^IMITATE_FDISK.*#IMITATE_FDISK=true#g" /usr/local/bin/fdisk ||
 	echo "Failed to set IMITATE_FDISK variable on /usr/local/bin/fdisk"
 }
+
+#Copy LIB library into /usr/local/bin/fdisk
+#sed -i "s#^LIBRARY_HERE#$(cat $SCRIPT_DIR/LIB)#g" /usr/local/bin/fdisk ||
+sed -i "/^LIBRARY_HERE/{r$SCRIPT_DIR/LIB
+d
+}" /usr/local/bin/fdisk ||
+{ echo "Failed to replace LIBRARY_HERE with LIB library"; [[ -e /usr/local/bin/fdisk ]] && rm -f /usr/local/bin/fdisk; exit 1; }
 
 #Check if sudo is on the system
 if which sudo &>/dev/null
